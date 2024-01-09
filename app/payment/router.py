1283e8369883633862e29431d9b4caa77fd8745e
coderunner86 <java@unicauca.edu.com>
2024-01-09 16:50:26 -0500

    feat: Add confirm payment route

--- app/payment/router.py
+++ app/payment/router.py
@@ -1,7 +1,8 @@
 from fastapi import APIRouter, HTTPException, Depends
 
-from app.payment.services import CreatePayment, UpdatePayment, PaymentService
+from app.payment.services import CreatePayment, UpdatePayment, PaymentService, ConfirmPayment
 
+import requests
 
 payment_service = PaymentService
 
@@ -13,22 +14,34 @@ router = APIRouter(
 
 
 @router.get("/{id:int}")
-async def get_user(id: int, payment_service: PaymentService = Depends()):
+async def get_payment(id: int, payment_service: PaymentService = Depends()):
     return await payment_service.find_payment(payment_id=id)
 
 @router.get("")
-async def get_all_products(payment_service: PaymentService = Depends()):
+async def get_all_payments(payment_service: PaymentService = Depends()):
     return await payment_service.find_all_payment()
 
 @router.post("/")
-async def create_paymemt(payment: CreatePayment, payment_service: PaymentService= Depends()):
+async def create_payment(payment: CreatePayment, payment_service: PaymentService= Depends()):
     try:
         new_payment = await payment_service.create_payment(payment)
         return new_payment
     
     except Exception as ex:
         raise HTTPException(status_code=500, detail=f"Fuck! Internal Server Error: {str(ex)}")
+
+
+@router.post("/confirm")
+async def stripe_payment_confirm(payment_intent_id: str, payment_service: PaymentService = Depends()):
+    payment_method = 'pm_card_visa'
+    result = await payment_service.stripe_payment_confirm(payment_intent_id, payment_method)
+    status_code = 200
+    if status_code != 200:
+        raise HTTPException(status_code=400, detail=result)
+    return result
     
+    # pass
+
 
 @router.put("/{id}")
 async def update_payment(id: int, payment: UpdatePayment, payment_service:PaymentService = Depends()):
