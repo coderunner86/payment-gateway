c5e9456c6e9c8b8c9cf60ec07e9767737dbfdfe7
coderunner86 <java@unicauca.edu.com>
2024-01-14 16:40:15 -0500

    feat(auth): Implement password validation during sign-in process

--- app/login/router.py
+++ app/login/router.py
@@ -9,8 +9,10 @@ from starlette.responses import Response
 from app.auth.jwt_manager import create_token
 from app.stripe_integration.stripe_users import get_customers_info
 from app.login.services import UserLogin
-
+from app.user.services import UserService
 from app.settings.database import database
+
+from app.auth.hashing import Hasher
 
 templates = Jinja2Templates(directory="app/templates")
 router = APIRouter(
@@ -43,18 +45,23 @@ Input parameters:
 @router.post('/login')
 async def login(email: str = Form(...), password: str = Form(...)):
     user = UserLogin(email=email, password=password)
-    users_info = await get_customers_info()
-    print('users_info', users_info)
-    if not users_info:
-        raise HTTPException(status_code=400, detail="Incorrect email or password")
-
-    if user.email in [customer_info["email"] for customer_info in users_info.values()]:
-        token: str = create_token(user.dict())
-        print(token)
-        response = Response('/login')
-        response.set_cookie(key="token", value=token, httponly=True, secure=True)
-        redirect_response = RedirectResponse(url='/api/users/dashboard', status_code=303)
-        redirect_response.set_cookie(key="token", value=token, httponly=True, secure=True)
-        return redirect_response 
+    retrieve_user = await UserService().find_user_by_email(email=user.email)
+    if not retrieve_user:
+        found = False #to check if email exists in the database or not, if not then raise an exception to break
+        raise HTTPException(status_code=400, detail="Email not found")
     else:
-        raise HTTPException(status_code=404, detail="Email not found")
+        verified = Hasher.verify_password(plain_password=user.password, hashed_password=retrieve_user.password)
+        print(verified)
+        found = True
+        if found and verified:
+            print('Welcome',retrieve_user.email)
+            token: str = create_token(user.dict())
+            print(token)
+            response = Response('/login')
+            response.set_cookie(key="token", value=token, httponly=True, secure=True)
+            redirect_response = RedirectResponse(url='/api/users/dashboard', status_code=303)
+            redirect_response.set_cookie(key="token", value=token, httponly=True, secure=True)
+            return redirect_response 
+        else:
+            raise HTTPException(status_code=400, detail="Invalid credentials")
+        
