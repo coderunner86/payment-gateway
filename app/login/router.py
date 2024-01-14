c108ae400d6f37032b4ed395c56c0d7f0e7afba1
coderunner86 <java@unicauca.edu.com>
2024-01-14 14:31:00 -0500

    docs(login): Add documentations for login routes

--- app/login/router.py
+++ app/login/router.py
@@ -1,5 +1,7 @@
+
 from fastapi import APIRouter, Request, Form
 from fastapi import HTTPException
+
 from fastapi.responses import HTMLResponse, RedirectResponse
 from starlette.templating import Jinja2Templates
 from starlette.responses import Response
@@ -8,17 +10,36 @@ from app.auth.jwt_manager import create_token
 from app.stripe_integration.stripe_users import get_customers_info
 from app.login.services import UserLogin
 
+from app.settings.database import database
+
 templates = Jinja2Templates(directory="app/templates")
 router = APIRouter(
     prefix="/users",
     tags=["users"],
 )
 
+"""
+Route: GET /users/login
+
+Description: Renders the login form in HTML.
+Successful response: HTMLResponse with the content of the login form.
+Input parameters:
+  - request: Request object of type Request from FastAPI used to receive the HTTP request.
+"""
 @router.get('/login', response_class=HTMLResponse)
 async def login_form(request: Request):
     return templates.TemplateResponse("login.html", {"request": request})
 
-    
+
+"""
+Route: POST /users/login
+
+Description: Processes the login request and performs user authentication.
+Successful response: RedirectResponse that redirects the user to the dashboard page.
+Input parameters:
+  - email: String representing the user's email.
+  - password: String representing the user's password.
+"""    
 @router.post('/login')
 async def login(email: str = Form(...), password: str = Form(...)):
     user = UserLogin(email=email, password=password)
