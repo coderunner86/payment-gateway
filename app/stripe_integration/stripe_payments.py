24668ea74dad091875aaf75f2046f5dbe88932f3
coderunner86 <java@unicauca.edu.com>
2024-01-15 12:51:53 -0500

    feature: Add get payment links method

--- app/stripe_integration/stripe_payments.py
+++ app/stripe_integration/stripe_payments.py
@@ -10,6 +10,7 @@ env_path = os.path.join(".", ".env")
 load_dotenv(dotenv_path=env_path)
 stripe.api_key = os.getenv("STRIPE_SECRET_KEY")
 
+### List Payments ###
 async def get_payment_info():
     try:
         payments_info = stripe.PaymentIntent.list()
@@ -19,5 +20,21 @@ async def get_payment_info():
     except Exception as e:
         return {"error": str(e)}
     
+  
+### List Payment Links ###       
+async def get_payment_links():
+        try:
+            payment_links = stripe.PaymentLink.list(limit=100, active=True)
+            productos = stripe.Product.list(limit=100).data 
 
+            links_y_productos = {}
+            productos_invertidos = productos[::-1]
+            
+            for index, link in enumerate(payment_links.data):
+                producto = productos_invertidos[index]
+                links_y_productos[link.url] = producto.name
+            return links_y_productos
 
+        except Exception as e:
+            return {"error": str(e)}
+ 
