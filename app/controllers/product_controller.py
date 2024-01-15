79fa672cc03e96650dfc9838149e307b58b06a98
coderunner86 <java@unicauca.edu.com>
2024-01-15 12:48:36 -0500

    chore: format code

--- app/controllers/product_controller.py
+++ app/controllers/product_controller.py
@@ -2,17 +2,18 @@ from fastapi import APIRouter, Request, Depends
 from fastapi.responses import HTMLResponse
 from starlette.templating import Jinja2Templates
 
-#stripe imports
+# stripe imports
 from app.stripe_integration.stripe_products import get_payment_links
 from app.middlewares.jwt_handler import JWTBearer
-templates = Jinja2Templates(directory="app/templates")
-router = APIRouter(
-    prefix="/products",
-    tags=["products"]
-)
-# ,  dependencies=[Depends(JWTBearer())]
-@router.get("/catalog",response_class=HTMLResponse)
-async def payment_links(request: Request):
-        products_links =  await get_payment_links()
-        return templates.TemplateResponse("catalog.html",context={"request":request, "products_links": products_links})
 
+templates = Jinja2Templates(directory="app/templates")
+router = APIRouter(prefix="/products", tags=["products"])
+
+
+# ,  dependencies=[Depends(JWTBearer())]
+@router.get("/catalog", response_class=HTMLResponse)
+async def payment_links(request: Request):
+    products_links = await get_payment_links()
+    return templates.TemplateResponse(
+        "catalog.html", context={"request": request, "products_links": products_links}
+    )
