9d995a20403deb7d7e9bec30d9c38cf60c4621a9
coderunner86 <java@unicauca.edu.com>
2024-01-18 17:23:11 -0500

    refactor: Improve logic to register new user

--- app/register/router.py
+++ app/register/router.py
@@ -1,11 +1,15 @@
 from fastapi import APIRouter, Form, Request
 from fastapi.responses import HTMLResponse
 from starlette.templating import Jinja2Templates
-from starlette.responses import Response
+from starlette.responses import RedirectResponse
 
 from app.auth.jwt_manager import create_token
+from app.helpers.session import create_session
+
 from app.user.services import UserService
 from app.register.services import UserRegister
+
+
 
 templates = Jinja2Templates(directory="app/templates")
 
@@ -26,17 +30,27 @@ async def register(
     ):
         
             user = UserRegister(name=name, email = email, password = password)
-            print(user)
+            print("the user we will create", user)
             retrieve_user = await UserService().find_user_by_email(email=user.email)
             if retrieve_user == None:
+                users_id = await UserService().find_all_user()
+                last_user_id = users_id[-1].id
+                uid = last_user_id + 1
+                session_id = create_session(uid)
                 token: str = create_token(user.dict())
-                response = Response('/register')
+                response = RedirectResponse(url="/dashboard", status_code=303)
+                response.set_cookie(
+                key="session_id", value=session_id, httponly=True, secure=True
+            )
                 response.set_cookie(key="token", value=token, httponly=True, secure=True)
+                
                 try: 
                     result = await UserService().create_user(user)
-                    return  result
+                    print("This is the result when we create a user", result)
+                    return  response
                 except Exception as ex:
-                    return {"error": str(ex)}
+                    return(f'{"This error is when we try to create the user": str(ex)}')
+                    
             elif retrieve_user.email == user.email:
                 print("retrieve_user",retrieve_user.email)
                 exist = retrieve_user.email
