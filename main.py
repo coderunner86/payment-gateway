d0d532efd3063130ed3aa7bdba6ac7bba2d9dec8
coderunner86 <java@unicauca.edu.com>
2024-01-19 13:14:06 -0500

    feat(main): add descriptions to docs endpoints

--- main.py
+++ main.py
@@ -4,7 +4,6 @@ import uvicorn
 
 from fastapi import FastAPI, Request
 
-
 from dotenv import load_dotenv
 
 from app.settings.database import database
@@ -19,7 +18,7 @@ from fastapi.templating import Jinja2Templates
 
 from app.middlewares.error_handler import ErrorHamdler
 
-#stripe imports
+# stripe imports
 from app.stripe_integration.stripe_users import get_customers_info
 from app.stripe_integration.stripe_products import get_payment_links
 from app.stripe_integration.stripe_payments import get_payment_info
@@ -29,36 +28,54 @@ from app.register.router import register
 env_path = os.path.join(".", ".env")
 load_dotenv(dotenv_path=env_path)
 
-app = FastAPI()
-
+app = FastAPI(
+    title="BookAPI",
+    description="RESTful CRUD API for ebook site management with Stripe integration.",
+    version="1.0.0",
+    openapi_tags=[
+        {
+            "name": "users",
+            "description": "Endpoints for managing users on the site.",
+        },
+        {
+            "name": "products",
+            "description": "Endpoints for handling products with Stripe.",
+        },
+        {
+            "name": "payments",
+            "description": "Endpoints for handling payments with Stripe.",
+        },
+    ],
+)
 app.mount("/static", StaticFiles(directory="app/static"), name="static")
-
 app.add_middleware(ErrorHamdler)
 
 templates = Jinja2Templates(directory="app/templates")
 register_templates = Jinja2Templates(directory="app/register")
 stripe.api_key = os.getenv("STRIPE_SECRET_KEY")
 
+
+@app.get("/register", response_class=HTMLResponse)
+def register_form(request: Request):
+    response = register(request)
+    return templates.TemplateResponse("register.html", {"request": request})
+
+
+@app.get("/login", response_class=HTMLResponse)
+def login_form(request: Request):
+    return templates.TemplateResponse("login.html", {"request": request})
+
+
 @app.get("/dashboard", response_class=HTMLResponse)
 async def customers_dashboard(request: Request):
     session_id = request.cookies.get("session_id")
     token = request.cookies.get("token")
     if not token or not session_id:
-        return RedirectResponse(url='/login', status_code=303)
+        return RedirectResponse(url="/login", status_code=303)
     users = await get_customers_info()
-    return templates.TemplateResponse("dashboard.html", context={"request": request, "users": users})
-
-@app.get("/thankyou", response_class=HTMLResponse)
-def customers_dashboard(request: Request):
-    token = request.cookies.get("token")
-    session_id = request.cookies.get("session_id")
-    if not token or not session_id:
-        return RedirectResponse(url='/login', status_code=303)
-    return templates.TemplateResponse("thankyou.html", context={"request": request})
-
-@app.get('/login', response_class=HTMLResponse)
-def login_form(request: Request):
-    return templates.TemplateResponse("login.html", {"request": request})
+    return templates.TemplateResponse(
+        "dashboard.html", context={"request": request, "users": users}
+    )
 
 
 @app.get("/recommendation", response_class=HTMLResponse)
@@ -66,43 +83,64 @@ async def payment_links(request: Request):
     token = request.cookies.get("token")
     session_id = request.cookies.get("session_id")
     if not token or not session_id:
-        return RedirectResponse(url='/login', status_code=303)
+        return RedirectResponse(url="/login", status_code=303)
 
-    links_y_productos, productos = await get_payment_links()
+    links_y_productos = await get_payment_links()
 
     return templates.TemplateResponse(
-        "recommendation.html", 
+        "recommendation.html",
         context={
-            "request": request, 
+            "request": request,
             "links_y_productos": links_y_productos,
-            "productos": productos,
-        }
+        },
     )
+
+
+@app.get("/books", response_class=HTMLResponse)
+async def payment_links(request: Request):
+    token = request.cookies.get("token")
+    session_id = request.cookies.get("session_id")
+    if not token or not session_id:
+        return RedirectResponse(url="/login", status_code=303)
+
+    links_y_productos = await get_payment_links()
+
+    return templates.TemplateResponse(
+        "catalog.html",
+        context={
+            "request": request,
+            "links_y_productos": links_y_productos,
+        },
+    )
+
 
 @app.get("/payments", response_class=HTMLResponse)
 async def payment_info(request: Request):
     session_id = request.cookies.get("session_id")
     token = request.cookies.get("token")
     if not token or not session_id:
-        return RedirectResponse(url='/login', status_code=303)
+        return RedirectResponse(url="/login", status_code=303)
     products = await get_payment_info()
     return templates.TemplateResponse(
         "payments.html", context={"request": request, "products": products}
     )
 
+
+@app.get("/thankyou", response_class=HTMLResponse)
+def customers_dashboard(request: Request):
+    token = request.cookies.get("token")
+    session_id = request.cookies.get("session_id")
+    if not token or not session_id:
+        return RedirectResponse(url="/login", status_code=303)
+    return templates.TemplateResponse("thankyou.html", context={"request": request})
+
+
 @app.get("/logout")
 def logout(request: Request):
-    response = RedirectResponse(url='/login', status_code=303)
+    response = RedirectResponse(url="/login", status_code=303)
     response.delete_cookie("token")
     response.delete_cookie("session_id")
     return response
-
-
-@app.get('/register', response_class=HTMLResponse)
-def register_form(request: Request):
-    response = register(request)
-    return templates.TemplateResponse("register.html", {"request": request})
-
 
 
 @app.on_event("startup")
