c8369f78f6dff7cc01cea816a7b37314a184ee63
coderunner86 <java@unicauca.edu.com>
2024-01-19 13:13:23 -0500

    refactor: add image list inverted to export in links_y_productos

--- app/stripe_integration/stripe_products.py
+++ app/stripe_integration/stripe_products.py
@@ -19,20 +19,25 @@ async def get_payment_links():
             links = [link.url for link in stripe.PaymentLink.list(limit=100, active=True).data]
             productos = [producto.name for producto in stripe.Product.list(limit=100).data]        
 
-            productos_invertidos = list.reverse(productos)
 
             linarray = [i for i in range(1, len(links)+1)]
             shift_linarray = [i-1 for i in linarray]
-            
+
+            productos_invertidos = list.reverse(productos)
             shift_productos = [productos[i-2] for i in shift_linarray]
             productos_invertidos = shift_productos[::1]
-            images = [product.images for product in stripe.Product.list(limit=100).data]  
-
+            
+            images = [product.images for product in stripe.Product.list(limit=100).data]
+            images_invertidos = list.reverse(images)  
+            shift_images = [images[i-2] for i in shift_linarray]
+            images_invertidos = shift_images[::1]    
+            
             desc = [product.description for product in stripe.Product.list(limit=100).data]  
             desc_invertidos = list.reverse(desc)
             shift_desc = [desc[i-2] for i in shift_linarray]
             desc_invertidos = shift_desc[::1]                
-            links_y_productos = [(link, nombre, imagen, desc) for link, nombre, imagen, desc in zip(links, productos_invertidos, images, desc_invertidos)]
+
+            links_y_productos = [(link, nombre, imagen, desc) for link, nombre, imagen, desc in zip(links, productos_invertidos, images_invertidos, desc_invertidos)]
             
             return links_y_productos
 
