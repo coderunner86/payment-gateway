ae26a9001d3f7f574e48780d5f9dba5af1081f1b
coderunner86 <java@unicauca.edu.com>
2024-01-22 03:17:47 -0500

    feat(routers): add gpt router

--- app/settings/routers.py
+++ app/settings/routers.py
@@ -1,13 +1,14 @@
 from app.user.router import router as user_router
 from app.product.router import router as product_router
 from app.payment.router import router as payment_router
-
+from app.gpt.router import router as gpt_router
 from app.login.router import router as login_router
 from app.register.router import router as register_router
 routers = [
     user_router,
     product_router,
     payment_router,
+    gpt_router,
     login_router,
     register_router,
 ]
