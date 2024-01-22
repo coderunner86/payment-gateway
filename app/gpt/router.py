7858b8e231e79409e67e226f3699418a127c6dd9
coderunner86 <java@unicauca.edu.com>
2024-01-22 13:31:21 -0500

    update service and route

--- app/gpt/router.py
+++ app/gpt/router.py
@@ -1,16 +1,8 @@
-
 from fastapi import APIRouter, HTTPException, Request
 from app.gpt.services import GptService, UserQuestion
-from app.helpers.session import get_user_id_from_session
-import os
-from dotenv import load_dotenv
 
-load_dotenv() 
-
-API_KEY = os.getenv("API_KEY")
-gpt_service = GptService(API_KEY)
-
-router = APIRouter( prefix="/gpt", tags=["gpt"])
+router = APIRouter(prefix="/gpt", tags=["gpt"])
+gpt_service = GptService()
 
 @router.post("/ask-gpt")
 async def ask_gpt_endpoint(request: Request, user_question: UserQuestion):
@@ -19,5 +11,3 @@ async def ask_gpt_endpoint(request: Request, user_question: UserQuestion):
         return result
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
-
-
--- app/gpt/services.py
+++ app/gpt/services.py
@@ -1,53 +1,61 @@
-import requests
+import httpx
 from fastapi import HTTPException, Request
+from app.settings.database import database
 from pydantic import BaseModel
+from typing import Optional
 import os
 from dotenv import load_dotenv
 from app.helpers.session import get_user_id_from_session
-load_dotenv() 
+load_dotenv()
 
 class UserQuestion(BaseModel):
     question: str
-    gptResponse: str
-    userId: int    
-
 
 API_KEY = os.getenv("API_KEY")
+
 class GptService:
-    def __init__(self, API_KEY):
+    def __init__(self):
         self.API_KEY = API_KEY
+        self.repository = database
 
-    def ask_gpt(self, question: str) -> str:
-        try:
-            response = requests.post(
-                'https://api.openai.com/v1/engines/gpt-3.5-turbo-instruct/completions',
-                headers={
-                    'Authorization': f'Bearer {self.API_KEY}',
-                    'Content-Type': 'application/json',
-                },
-                json={
-                    'prompt': question,
-                    'temperature': 0.7,
-                    'max_tokens': 60,
-                }
-            )
-            response.raise_for_status()
-            return response.json()['choices'][0]['text']
-        except requests.RequestException as e:
-            raise HTTPException(status_code=400, detail=str(e))
+    async def ask_gpt(self, question: str) -> str:
+        async with httpx.AsyncClient() as client:
+            try:
+                response = await client.post(
+                    'https://api.openai.com/v1/engines/gpt-3.5-turbo-instruct/completions',
+                    headers={
+                        'Authorization': f'Bearer {self.API_KEY}',
+                        'Content-Type': 'application/json',
+                    },
+                    json={
+                        'prompt': question,
+                        'temperature': 0.7,
+                        'max_tokens': 60,
+                    }
+                )
+                response.raise_for_status()
+                return response.json()['choices'][0]['text']
+            except httpx.RequestError as e:
+                raise HTTPException(status_code=400, detail=f"HTTP request failed: {e}")
 
-    async def create_question(self, request: Request,user_question: UserQuestion):
+    async def create_question(self, request: Request, userquestion: UserQuestion):
         try:
             session_id = request.cookies.get("session_id")
             print("session_id", session_id)
             user_id = get_user_id_from_session(session_id)
-            print("user_id", user_id)
-            gpt_response = self.ask_gpt(user_question.question)
-            print("gpt_response", gpt_response)
-            data = {"question": user_question.question, "gpt_response": user_question.gptResponse, "user_id": user_id}
+            data = userquestion.dict()
+            data["question"] = userquestion.question
+            data["user_id"] = user_id
+            # gpt_response = self.ask_gpt(self, userquestion.question)
+            gpt_response = "Grandioso"
+            data["gptResponse"] = gpt_response
+            data = {"question": userquestion.question,"gptResponse": gpt_response, "user_id": user_id}
+            print(userquestion)
+            print(type(userquestion.dict()))
             print("data", data)
-            result = await self.repository.user_question.create(data=data)
+            print(type(data))          
+            result = await self.repository.userquestion.create(data=userquestion.dict()) #.gpt.create(data=data)
             print("result", result)
             return result
         except Exception as e:
-            raise HTTPException(status_code=500, detail=str(e))
+            raise HTTPException(status_code=500, detail=f"Error creating question: {e}")
