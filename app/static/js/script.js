19f6d0f51ad58a926e7bb5d9ed1780497c7629f8
coderunner86 <java@unicauca.edu.com>
2024-01-23 11:55:48 -0500

    update static js

--- app/static/js/script.js
+++ app/static/js/script.js
@@ -1,12 +1,10 @@
 async function askGPT(question) {
     try {
         
-        // const session_id = document.getElementById('session_id').value;
         const response = await fetch('http://localhost:8000/api/gpt/ask-gpt', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json'
-                // ,'session_id': session_id
             },
             body: JSON.stringify({ question: question})
         });
@@ -16,52 +14,17 @@ async function askGPT(question) {
         }
 
         const data = await response.json();
-        console.log(data);
+        console.log(data)
+        process_data = tohiperLink(data)
+        document.getElementById('gptResponse').innerHTML = process_data 
  
     } catch (error) {
         console.error('Error:', error);
     }
 }
-
-
-//  async function askGPT() {
-//             try {
-//                 const question = document.getElementById('question').value;
-
-
-//                 var products = await fetch(`/api/products/all_products`, {
-//                     method: 'GET',
-//                     headers: {
-//                         'Content-Type': 'application/json'
-//                     }
-//                 })
-//                     .then(response => response.json())
-//                     .catch(error => {
-//                         console.log('error', error);
-//                         return [];
-//                     });
-
-//                 var productDescriptions = products.map(product => `${product.name}: ${product.description}`).join(", ");
-
-//                 const promptText = `${question}. Aquí hay algunos libros disponibles: ${productDescriptions}. Basado en esto, ¿cuál recomendarías?`;
-
-//                 const response = await fetch('https://api.openai.com/v1/engines/gpt-3.5-turbo-instruct/completions', {
-//                     method: 'POST',
-//                     headers: {
-//                         'Content-Type': 'application/json',
-//                         'Authorization': 'Bearer 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'
-//                     },
-//                     body: JSON.stringify({
-//                         prompt: promptText,
-//                         temperature: 0.7,
-//                         max_tokens: 60
-//                     })
-//                 });
-
-//                 const data = await response.json();
-//                 document.getElementById('gptResponse').innerText = data.choices[0].text;
-//             } catch (error) {
-//                 console.error('Error:', error);
-//                 document.getElementById('gptResponse').innerText = 'Error al obtener respuesta de GPT.';
-//             }
-//         }
+function tohiperLink(response) {
+    const urlRegex = /(https?:\/\/[^\s]+)/g;
+    return response.replace(urlRegex, function(url) {
+        return `<a href="${url}" target="_blank">${url}</a>`;
+    });
+}
