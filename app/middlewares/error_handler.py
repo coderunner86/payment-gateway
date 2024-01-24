52d8435005e7e442b1d2f18e9af61560599c5838
coderunner86 <java@unicauca.edu.com>
2024-01-23 21:30:39 -0500

    refactor(middleware): enhance error handling

--- app/middlewares/error_handler.py
+++ app/middlewares/error_handler.py
@@ -1,7 +1,9 @@
 from starlette.middleware.base import BaseHTTPMiddleware
-from fastapi import FastAPI, Request, Response
-from fastapi.responses import JSONResponse
+from fastapi import FastAPI, Request, Response, HTTPException
+from fastapi.responses import JSONResponse, HTMLResponse
+from starlette.templating import Jinja2Templates
 
+templates = Jinja2Templates(directory="app/templates")
 class ErrorHamdler(BaseHTTPMiddleware):
     def __init__(self, app: FastAPI) -> None:
         super().__init__(app)
@@ -9,6 +11,9 @@ class ErrorHamdler(BaseHTTPMiddleware):
     async def dispatch(self, request: Request, call_next) -> Response | JSONResponse:
         try: 
             return await call_next(request)
+        except HTTPException as exc:
+            if exc.status_code == 401:
+                return templates.TemplateResponse("login.html", {"request": request, "alert": exc.detail})
         except Exception as ex:
             return JSONResponse(status_code=500, content={'error':str(ex)})
     
